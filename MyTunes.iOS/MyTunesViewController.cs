9090856943e653a798f0973934fe8fa9fdcd5fa0
Dan Manning <dmanning@datafinch.com>
2014-10-06 18:38:04 -0400

    initial commit

---
+++ MyTunes.iOS/MyTunesViewController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using MonoTouch.UIKit;
+
+namespace MyTunes
+{
+	public class MyTunesViewController : UITableViewController
+	{
+		public override void ViewDidLayoutSubviews()
+		{
+			base.ViewDidLayoutSubviews();
+
+			if (UIDevice.CurrentDevice.CheckSystemVersion(7,0)) {
+				TableView.ContentInset = new UIEdgeInsets (20, 0, 0, 0);
+			}
+		}
+
+		public async override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+            //TableView.Source = new ViewControllerSource<string>(TableView) {
+            //    DataSource = new string[] { "One", "Two", "Three" },
+            //};
+
+            var data = await SongLoader.Load();
+            TableView.Source = new ViewControllerSource<Song>(TableView) { 
+                DataSource = data.ToList(),
+                TextProc = a => a.Name,
+                DetailTextProc = a => a.Artist + " - " + a.Album
+            };
+		}
+	}
+
+}
+
